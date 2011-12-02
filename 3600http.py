e234338b0d5ae7be7791bd9f5971f9e833bf3bd4
Zane Whitney <whitneyz@ccs.neu.edu>
2011-12-02 15:52:43 -0500

    test

--- 3600http.py
+++ 3600http.py
@@ -9,6 +9,8 @@ import sys
 import threading
 import argparse
 
+#DERP, I AM TESTING SOMETHING...
+
 class Header:
 	def __init__(self, date, server, connection, contentLength, contentType):
 		self.date = date
@@ -16,17 +18,17 @@ class Header:
 		self.connection = connection
 		self.contentLength = contentLength
 		self.contentType = contentType
-		
-		
+
+
 def main (directory, port=8080):
 	# main definition here
-	
+
 	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server_socket.bind(("", port))
 	server_socket.listen(5)
-	
+
 	print ("Waiting for requests on port", port)
-	
+
 	while 1:
 		client_socket, address = server_socket.accept()
 		print ("id CONNECT") # note - need to assign id
@@ -37,8 +39,8 @@ def main (directory, port=8080):
 		# check mime type - see notes.txt - need to check file extension
 		# if it can be found, print "id DELIVERED" and return requested file
 		# if it can't be found, print "HTTP/1.0 404 Not Found"
-		
-		# when returning the requested file: 
+
+		# when returning the requested file:
 		# print "HTTP/1.0 200 OK"
 		# Header.date = currentDate
 		# Header.server = serverName
@@ -46,7 +48,7 @@ def main (directory, port=8080):
 		# Header.contentLength = numberOfBytesInContent
 		# Header.contentType = header for mime type - see above and notes.txt
 		# return Header, return requested file
-		
+
 
 if __name__ == "__main__":
 	if (len(sys.argv) < 4):
