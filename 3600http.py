6fa622a682d283f33722d5382ab91d97ddb421cc
Zane Whitney <whitneyz@ccs.neu.edu>
2011-12-05 17:37:24 -0500

    Added code to recieve an HTTP request on a port.

--- 3600http.py
+++ 3600http.py
@@ -15,31 +15,34 @@ class Header:
 		self.connection = connection
 		self.contentLength = contentLength
 		self.contentType = contentType
-		
-		
+
+
 def main (directory, port=8080):
 	# main definition here
-	
+
 	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	server_socket.bind(("", port))
-	
+
 	print ("Waiting for requests on port", port)
-	
+
 	while 1:
 		server_socket.listen(5)
 		client_socket, address = server_socket.accept()
+
+                print ("Connected: ", client_socket, address)
+
+                httpreq = client_socket.recv(4096)
+                print ("http request: ", httpreq)
+
 		print ("id CONNECT") # note - need to assign id
-		file = client_socket.makefile('rw', 1)
-		line = file.readline().strip()
 		print ("id REQUEST") # note - need to assign id
-		file.write("HTTP/1.0 200 OK\n\n")
 		# need to check here if requested page can be found:
 		# check mime type - see notes.txt - need to check file extension
 		# if it can be found, print "id DELIVERED" and return requested file
 		# if it can't be found, print "HTTP/1.0 404 Not Found"
-		
-		# when returning the requested file: 
+
+		# when returning the requested file:
 		# print "HTTP/1.0 200 OK"
 		# Header.date = currentDate
 		# Header.server = serverName
@@ -47,10 +50,9 @@ def main (directory, port=8080):
 		# Header.contentLength = numberOfBytesInContent
 		# Header.contentType = header for mime type - see above and notes.txt
 		# return Header, return requested file
-		
-		file.close()
+
 		client_socket.close()
-		
+
 
 if __name__ == "__main__":
 	if (len(sys.argv) < 4):
